7fadc8ecae03e9f5a7adb2ce780262914fdfc06e
mShubham18 <shubhuu1804@gmail.com>
2025-03-30 00:35:07 +0530

    Integrated basic endpoints

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/service")
+def service():
+    return render_template("service.html")
+@app.route("/plans")
+def plans():
+    return render_template("plans.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
